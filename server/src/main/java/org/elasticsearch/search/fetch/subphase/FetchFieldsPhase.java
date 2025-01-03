fe14fd809cfebf9ad9e22c5cf5f1cfa0e94950d1
Mike Pellegrini <mike.pellegrini@elastic.co>
2025-01-03 16:13:50 -0500

    Check that field is mapped

--- server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchFieldsPhase.java
+++ server/src/main/java/org/elasticsearch/search/fetch/subphase/FetchFieldsPhase.java
@@ -71,7 +71,8 @@ public final class FetchFieldsPhase implements FetchSubPhase {
                         .stream()
                         .filter(
                             fieldAndFormat -> (searchExecutionContext.isMetadataField(fieldAndFormat.field) == false
-                                || searchExecutionContext.getFieldType(fieldAndFormat.field).isStored() == false
+                                || (searchExecutionContext.isFieldMapped(fieldAndFormat.field)
+                                    && searchExecutionContext.getFieldType(fieldAndFormat.field).isStored() == false)
                                 || IdFieldMapper.NAME.equals(fieldAndFormat.field)
                                 || SourceFieldMapper.NAME.equals(fieldAndFormat.field))
                         )
@@ -87,6 +88,7 @@ public final class FetchFieldsPhase implements FetchSubPhase {
                     continue;
                 }
                 if (searchExecutionContext.isMetadataField(fieldAndFormat.field)
+                    && searchExecutionContext.isFieldMapped(fieldAndFormat.field)
                     && searchExecutionContext.getFieldType(fieldAndFormat.field).isStored()) {
                     fetchContextMetadataFields.add(fieldAndFormat);
                 }
