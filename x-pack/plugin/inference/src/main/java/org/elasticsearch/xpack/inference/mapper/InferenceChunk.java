4b9edc59f8b9efbc662110a3c27f0432328fa498
Mike Pellegrini <mike.pellegrini@elastic.co>
2026-03-16 14:23:43 -0400

    Added InferenceChunk and InferenceResult abstract classes

---
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/InferenceChunk.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.mapper;
+
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.xcontent.ToXContentObject;
+import org.elasticsearch.xcontent.XContentType;
+
+import java.util.Objects;
+
+public abstract class InferenceChunk<T extends InferenceChunk<T>> implements ToXContentObject {
+    protected final BytesReference rawEmbeddings;
+    protected final XContentType contentType;
+
+    public InferenceChunk(BytesReference rawEmbeddings, XContentType contentType) {
+        this.rawEmbeddings = Objects.requireNonNull(rawEmbeddings);
+        this.contentType = Objects.requireNonNull(contentType);
+    }
+
+    protected abstract boolean doEquals(T other);
+
+    protected abstract int doHashCode();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        @SuppressWarnings("unchecked")
+        T that = (T) o;
+        return Objects.equals(rawEmbeddings, that.rawEmbeddings) && contentType == that.contentType && doEquals(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rawEmbeddings, contentType, doHashCode());
+    }
+}
---
+++ x-pack/plugin/inference/src/main/java/org/elasticsearch/xpack/inference/mapper/InferenceResult.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+package org.elasticsearch.xpack.inference.mapper;
+
+import org.elasticsearch.inference.ChunkingSettings;
+import org.elasticsearch.inference.MinimalServiceSettings;
+import org.elasticsearch.xcontent.ToXContentObject;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public abstract class InferenceResult<T extends InferenceChunk<T>, U extends InferenceResult<T, U>> implements ToXContentObject {
+    protected final String inferenceResult;
+    protected final MinimalServiceSettings modelSettings;
+    protected final ChunkingSettings chunkingSettings;
+    protected final Map<String, List<T>> chunks;
+
+    public InferenceResult(
+        String inferenceResult,
+        MinimalServiceSettings modelSettings,
+        ChunkingSettings chunkingSettings,
+        Map<String, List<T>> chunks
+    ) {
+        this.inferenceResult = inferenceResult;
+        this.modelSettings = modelSettings;
+        this.chunkingSettings = chunkingSettings;
+        this.chunks = chunks;
+    }
+
+    protected abstract boolean doEquals(U other);
+
+    protected abstract int doHashCode();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        @SuppressWarnings("unchecked")
+        U that = (U) o;
+        return Objects.equals(inferenceResult, that.inferenceResult)
+            && Objects.equals(modelSettings, that.modelSettings)
+            && Objects.equals(chunkingSettings, that.chunkingSettings)
+            && Objects.equals(chunks, that.chunks)
+            && doEquals(that);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inferenceResult, modelSettings, chunkingSettings, chunks, doHashCode());
+    }
+}
